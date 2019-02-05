697333c40c1dc2ad2f8110ed7f2682816abd21ed
Kyle Geske <stungeye@gmail.com>
2019-02-05 10:48:12 -0600

    Added the beer seed script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Beer.destroy_all
+
+50.times do
+  Beer.create(
+    name: Faker::Beer.unique.name,
+    alcohol: Faker::Beer.alcohol,
+    ibu: Faker::Beer.ibu,
+    price: Faker::Commerce.price(range = 2..20.0, as_string = false)
+  )
+end
+
+puts "Beers in DB: #{Beer.count}"
