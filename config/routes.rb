d3896e4edb4878cc8034e58850090487c2f2f748
Kyle Geske <stungeye@gmail.com>
2019-02-05 10:49:55 -0600

    Added the beers and search controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'search/results'
+  get 'beers/index'
+  get 'beers/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
