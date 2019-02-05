eee2a1fbd4ef9d6bfb9cbd1f9b02f542d9e71b1e
Kyle Geske <stungeye@gmail.com>
2019-02-05 10:58:13 -0600

    Implemented the beers controller's index action.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,6 +1,9 @@
 class BeersController < ApplicationController
   def index
+    @beers = Beer.order(:price)
   end
+  # So the @beers variable will be shared with
+  # the app/views/beers/index.html.erb
 
   def show
   end
