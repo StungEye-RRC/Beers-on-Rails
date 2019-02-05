0c631102b23f7dbfe49fe5dbb70aa53df89093e6
Kyle Geske <stungeye@gmail.com>
2019-02-05 11:36:01 -0600

    Implemented a partial for a beer and used it on a few pages.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,6 +5,10 @@ class BeersController < ApplicationController
   # So the @beers variable will be shared with
   # the app/views/beers/index.html.erb
 
+  # GET /beers/:id
   def show
+    @beer = Beer.find(params[:id])
   end
+  # The @beer variable will be shared with
+  # the app/views/beers/show.html.erb
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :beers, only: [:index]
+  resources :beers, only: [:index, :show]
 
   get 'search_results', to: 'search#results', as: 'search_results'
 
