1c03a55237176b7c5549671b7fbbd7db7a007eaa
Kyle Geske <stungeye@gmail.com>
2019-02-05 11:21:23 -0600

    Implemented a simple search form.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
   def results
+    @query = params[:q]
+    @beers = Beer.where('name LIKE ?', "%#{@query}%")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :beers, only: [:index]
 
+  get 'search_results', to: 'search#results', as: 'search_results'
+
   root to: 'beers#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
