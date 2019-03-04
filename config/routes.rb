f7502c055866ff28d92d22ea1bd909da9cd121ec
Kyle Geske <stungeye@gmail.com>
2019-03-04 10:31:12 -0600

    After adding active admin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :beers, only: [:index, :show]
 
   get 'search_results', to: 'search#results', as: 'search_results'
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,5 @@ Beer.destroy_all
 end
 
 puts "Beers in DB: #{Beer.count}"
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
