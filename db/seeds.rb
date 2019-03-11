3263c6a070dafca73f2dbb5b9ca0f292bc19415f
Kyle Geske <stungeye@gmail.com>
2019-03-11 10:23:01 -0500

    Added beer categories to the seed script and the beer partial.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Beer.destroy_all
+AdminUser.destroy_all
 
-50.times do
-  Beer.create(
-    name: Faker::Beer.unique.name,
-    alcohol: Faker::Beer.alcohol,
-    ibu: Faker::Beer.ibu,
-    price: Faker::Commerce.price(range = 2..20.0, as_string = false)
-  )
+5.times do
+  category = Category.create(name: Faker::Beer.unique.style)
+  10.times do
+    category.beers.create(
+      name: Faker::Beer.unique.name,
+      alcohol: Faker::Beer.alcohol,
+      ibu: Faker::Beer.ibu,
+      price: Faker::Commerce.price(range = 2..20.0, as_string = false)
+    )
+  end
 end
 
 puts "Beers in DB: #{Beer.count}"
